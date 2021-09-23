15e997328706e669c64f7fde527604405b838917
hassan mohamed <kokobosh@gmail.com>
2021-09-22 20:20:16 -0400

    installed packages

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Importing the necessary libraries
 
 import pickle
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model \
+import LogisticRegression
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
